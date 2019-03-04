39f682fec73ce9b77486e7d7087d1867eeffffa0
fatima awad <fatimamuk0@gmail.com>
2019-03-03 20:49:05 -0500

    react app created

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul className='navbar'>
+          <li><img src=""/></li>
+          <li><img src=""/></li>
+        </ul>
+
       </div>
     );
   }
