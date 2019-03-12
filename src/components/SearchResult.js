36059053d83574651f8d23ed6f3348ee46ca1dfc
fatima awad <fatimamuk0@gmail.com>
2019-03-11 23:27:39 -0400

    new component for search results

---
+++ src/components/SearchResult.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+ 
+
+class SearchResult extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+return (
+    <div>
+
+    </div>
+)
+    }
+}
+
+
+export default SearchResult; 
