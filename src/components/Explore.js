417d00854f5e9541f56b7acf9b4b5438c30ed9fc
fatima awad <fatimamuk0@gmail.com>
2019-03-11 23:23:16 -0400

    new component

---
+++ src/components/Explore.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+class Explore extends Component {
+    constructor(props){
+        super(props);
+    }
+
+
+render(){
+    return (
+        <div>
+hi this is explore :) 
+        </div>
+    );
+}
+
+
+
+
+
+}
+
+export default Explore;
